740cf194f51023c74370eb699ca2ce60fa6b4af3
Imikan10 <basseyimikan10@gmail.com>
2024-02-14 17:08:17 +0100

    using useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {names} from './data'
 
-function App() {
+
+const App = () => {
+  const [people, setPeople] = useState(names);
+  let number = 5;
+  const handle = () => {
+    setPeople([])  
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <section className='container'>
+        <p className='head'>{number} Birthdays Today</p>
+        {people.map((person) => {
+          const { id, name, age } = person
+          return (
+            <>
+              <article key={id} className='item'>
+                <h4>{name}</h4>
+                <p>{age} years</p>
+              </article>
+            </>
+          )
+        })}
+        <button className='btn' onClick={handle}>Clear All</button>
+      </section>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const names = [
+  {
+    id: 1,
+    name: 'Imikan Bassey',
+    age: 29,
+  },
+  {
+    id: 2,
+    name: 'Miracle Chegbene',
+    age: 32,
+  },
+  {
+    id: 3,
+    name: 'Olisa Anslem',
+    age: 36,
+  },
+  {
+    id: 4,
+    name: 'Ogunrombi Emmanuel',
+    age: 34,
+  },
+  {
+    id: 5,
+    name: 'Anuoluwapo Babalola',
+    age: 29,
+  },
+]
