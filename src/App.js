e5d9b10eeeb98acddcdafbd23029cf026887a1bf
Imikan10 <basseyimikan10@gmail.com>
2024-02-15 13:28:37 +0100

    finished work

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import {names} from './data'
 
 const App = () => {
   const [people, setPeople] = useState(names);
-  let number = 5;
+  const number = people.length;
   const handle = () => {
-    setPeople([])  
+    setPeople([]) 
   }
   return (
     <>
       <section className='container'>
         <p className='head'>{number} Birthdays Today</p>
         {people.map((person) => {
-          const { id, name, age } = person
+          const { id, name, age, img } = person
           return (
             <>
               <article key={id} className='item'>
-                <h4>{name}</h4>
-                <p>{age} years</p>
+                <img src={img} alt='' />
+                <div>
+                  <h4>{name}</h4>
+                  <p>{age} years</p>
+                </div>
               </article>
             </>
           )
--- src/data.js
+++ src/data.js
@@ -3,25 +3,30 @@ export const names = [
     id: 1,
     name: 'Imikan Bassey',
     age: 29,
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6h2uJCIs9GNeT8oLrGLaeCL290nJzWAF3m_PTbeYz7Q&s',
   },
   {
     id: 2,
     name: 'Miracle Chegbene',
     age: 32,
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj5w7hYktD4byIBek2BULUelcM6ybvsS5JBA3PzmA8pA&s',
   },
   {
     id: 3,
     name: 'Olisa Anslem',
     age: 36,
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrLUE91HU82SWr0y4AOgJ4hwozYbJqfsXFViUVmej2lA&s',
   },
   {
     id: 4,
     name: 'Ogunrombi Emmanuel',
     age: 34,
+    img: 'https://st3.depositphotos.com/1017228/18878/i/450/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg',
   },
   {
     id: 5,
     name: 'Anuoluwapo Babalola',
     age: 29,
+    img: 'https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8fDB8fHww',
   },
 ]
